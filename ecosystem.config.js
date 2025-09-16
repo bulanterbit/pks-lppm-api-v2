cc46826a87a6ee8714816ca29c4c001b9fea05c2
Bulan Terbit <jaconiah111@gmail.com>
2025-09-16 08:25:31 +0700

    test ci/cd deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,13 +3,10 @@ module.exports = {
     {
       name: "pks-api",
       script: "server.js",
-      // Jangan masukkan SECRET di sini kalau file ini di-commit!
-      env_development: {
-        NODE_ENV: "development",
-      },
+      instances: 1,
+      exec_mode: "fork",
       env_production: {
         NODE_ENV: "production",
-        // Variabel lain akan dibaca dari .env.production lewat dotenv-flow
       },
     },
   ],
